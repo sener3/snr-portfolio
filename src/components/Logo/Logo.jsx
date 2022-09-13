47ce63fbb08b08b7a84bbe4950e66f2916def0ab
sener21 <ali.sener@webmarc.io>
2022-09-13 22:00:45 +0300

    fix: logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,7 @@ import Typography from "../Typography/Typography";
 function Logo() {
     return (
         <Typography id="logo" className="logo" variant="p">
-            S <Typography variant="span">N</Typography>R
+            S<Typography variant="span">N</Typography>R
         </Typography>
     );
 }
